7e2cb299d3f3e043d8c5512c754ec094b992eeb8
mnfaizp <zynthkas@gmail.com>
2023-06-11 09:02:06 +0700

    use case: add user

---
+++ src/Applications/use_case/AddUserUseCase.js
@@ -0,0 +1,17 @@
+const CreateUser = require('../../Domains/CreateUser');
+
+class AddUserUseCase {
+  constructor({ userRepository, passwordHash }) {
+    this._userRepository = userRepository;
+    this._passwordHash = passwordHash;
+  }
+
+  async execute(useCasePayload) {
+    const createUser = new CreateUser(useCasePayload);
+    await this._userRepository.verifyAvailableEmail(createUser.email);
+    createUser.password = await this._passwordHash.hash(createUser.password);
+    return this._userRepository.addUser(createUser);
+  }
+}
+
+module.exports = AddUserUseCase;
---
+++ src/Applications/use_case/_test/AddUserUseCase.test.js
@@ -0,0 +1,50 @@
+const CreatedUser = require('../../../Domains/CreatedUser');
+const CreateUser = require('../../../Domains/CreateUser');
+const UserRepository = require('../../../Domains/users/UserRepository');
+const PasswordHash = require('../../security/PasswordHash');
+const AddUserUseCase = require('../AddUserUseCase');
+
+describe('AddUserUseCase', () => {
+  it('should orchestrating the add user action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      email: 'user_test@mail.com',
+      password: 'password',
+      name: 'user test',
+    };
+
+    const mockCreatedUser = new CreatedUser({
+      id: 'user-123',
+      name: useCasePayload.name,
+      email: useCasePayload.email,
+    });
+
+    // creating dependency of use case
+    const mockUserRepository = new UserRepository();
+    const mockPasswordHash = new PasswordHash();
+
+    // mocking needed function
+    mockUserRepository.verifyAvailableEmail = jest.fn(() => Promise.resolve());
+    mockPasswordHash.hash = jest.fn(() => Promise.resolve('encrypted_password'));
+    mockUserRepository.addUser = jest.fn(() => Promise.resolve(mockCreatedUser));
+
+    // creating use case instance
+    const addUserUseCase = new AddUserUseCase({
+      userRepository: mockUserRepository,
+      passwordHash: mockPasswordHash,
+    });
+
+    // Action
+    const createdUser = await addUserUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(createdUser).toStrictEqual(mockCreatedUser);
+    expect(mockUserRepository.verifyAvailableEmail).toBeCalledWith(useCasePayload.email);
+    expect(mockPasswordHash.hash).toBeCalledWith(useCasePayload.password);
+    expect(mockUserRepository.addUser).toBeCalledWith(new CreateUser({
+      email: useCasePayload.email,
+      password: 'encrypted_password',
+      name: useCasePayload.name,
+    }));
+  });
+});
