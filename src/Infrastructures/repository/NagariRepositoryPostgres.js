cf5f34680f06a76db266bb3a464305183d730346
mnfaizp <zynthkas@gmail.com>
2023-06-18 21:52:43 +0700

    nagari repository impl

---
+++ src/Infrastructures/repository/NagariRepositoryPostgres.js
@@ -0,0 +1,40 @@
+const InvariantError = require('../../Commons/exceptions/InvariantError');
+const ShowNagari = require('../../Domains/nagari/entities/ShowNagari');
+const NagariRepository = require('../../Domains/nagari/NagariRepository');
+
+class NagariRepositoryPostgres extends NagariRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async verifyAvailableNagariName(name) {
+    const query = {
+      text: 'SELECT name FROM nagari WHERE name = $1',
+      values: [name],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rowCount) {
+      throw new InvariantError('tidak dapat membuat nagari baru karena nama sudah digunakan');
+    }
+  }
+
+  async addNagari(createNagari) {
+    const { name } = createNagari;
+    const id = `nagari-${this._idGenerator()}`;
+
+    const query = {
+      text: 'INSERT INTO nagari(id, name) VALUES($1, $2) RETURNING id, name',
+      values: [id, name],
+    };
+
+    const result = await this._pool.query(query);
+
+    return new ShowNagari({ ...result.rows[0] });
+  }
+}
+
+module.exports = NagariRepositoryPostgres;
---
+++ src/Infrastructures/repository/_test/NagariRepositoryPostgres.test.js
@@ -0,0 +1,65 @@
+const pool = require('../../database/postgres/pool');
+const NagariTableTestHelper = require('../../../../tests/NagariTableTestHelper');
+const InvariantError = require('../../../Commons/exceptions/InvariantError');
+const CreateNagari = require('../../../Domains/nagari/entities/CreateNagari');
+const ShowNagari = require('../../../Domains/nagari/entities/ShowNagari');
+const NagariRepositoryPostgres = require('../NagariRepositoryPostgres');
+
+describe('NagariRepositoryPostgres', () => {
+  afterEach(async () => {
+    await NagariTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('addNagari function', () => {
+    it('should return created nagari correctly', async () => {
+      // Arrange
+      const createNagari = new CreateNagari({
+        name: 'Nagari Test',
+      });
+
+      const fakeIdGenerator = () => '123';
+      const nagariRepositoryPostgres = new NagariRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const createdNagari = await nagariRepositoryPostgres.addNagari(createNagari);
+
+      // Assert
+      const nagari = await NagariTableTestHelper.findNagariById('nagari-123');
+      expect(createdNagari).toStrictEqual(new ShowNagari({
+        id: 'nagari-123',
+        name: 'Nagari Test',
+      }));
+      expect(nagari).toHaveLength(1);
+    });
+  });
+
+  describe('verifyAvailableNagariName function', () => {
+    it('should throw InvariantError when nagari name not available', async () => {
+      // Arrange
+      const createNagari = new CreateNagari({
+        name: 'Nagari Test',
+      });
+      const nagariRepositoryPostgres = new NagariRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(nagariRepositoryPostgres.addNagari(createNagari))
+        .rejects.toThrowError(InvariantError);
+    });
+
+    it('should not throw InvariantError when nagari name available', async () => {
+      // Arrange
+      const createNagari = new CreateNagari({
+        name: 'Nagari Test',
+      });
+      const nagariRepositoryPostgres = new NagariRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(nagariRepositoryPostgres.addNagari(createNagari))
+        .resolves.not.toThrowError(InvariantError);
+    });
+  });
+});
