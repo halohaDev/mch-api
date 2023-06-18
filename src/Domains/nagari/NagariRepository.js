effe1744566afe9bcc678e22ea26b737bc47a0c6
mnfaizp <zynthkas@gmail.com>
2023-06-18 21:38:44 +0700

    repo interface

---
+++ src/Domains/nagari/NagariRepository.js
@@ -0,0 +1,11 @@
+class NagariRepository {
+  async addNagari() {
+    throw new Error('NAGARI_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyAvailableNagariName() {
+    throw new Error('NAGARI_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = NagariRepository;
---
+++ src/Domains/nagari/_test/NagariRepository.js
@@ -0,0 +1,12 @@
+const NagariRepository = require('../NagariRepository');
+
+describe('NagariRepository interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const nagariRepository = new NagariRepository();
+
+    // Action & Assert
+    await expect(nagariRepository.addNagari({})).rejects.toThrowError('NAGARI_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(nagariRepository.verifyAvailableNagariName('')).rejects.toThrowError('NAGARI_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
