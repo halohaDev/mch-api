3fe6da45e90418ca6e96edbda202773c27fc46f1
mnfaizp <zynthkas@gmail.com>
2023-06-21 04:41:41 +0700

    test full coverage

--- src/Domains/jorong/entities/_test/ShowJorong.test.js
+++ src/Domains/jorong/entities/_test/ShowJorong.test.js
@@ -11,6 +11,18 @@ describe('ShowJorong entities', () => {
     expect(() => new ShowJorong(payload)).toThrowError('SHOW_JORONG.NOT_CONTAIN_NEEDED_PROPERTY');
   });
 
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 'jorong-123',
+      name: 123,
+      nagariId: 'nagari-123',
+    };
+
+    // Action and Assert
+    expect(() => new ShowJorong(payload)).toThrowError('SHOW_JORONG.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
   it('should create showJorong object correctly', () => {
     // Arrange
     const payload = {
