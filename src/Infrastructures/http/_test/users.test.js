ae2c9538e778e246da894db66525802b3271e051
faiz <zynthkas@gmail.com>
2023-12-19 08:38:52 +0700

    remove console log

--- src/Infrastructures/http/_test/users.test.js
+++ src/Infrastructures/http/_test/users.test.js
@@ -367,7 +367,6 @@ describe('HTTP server - users', () => {
 
       // Assert
       const responseJson = JSON.parse(response.payload);
-      console.log(responseJson);
       expect(response.statusCode).toEqual(200);
       expect(responseJson.status).toEqual('success');
       expect(responseJson.data).toBeDefined();
