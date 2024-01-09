5d5e1307f04ef2f0cc0ea9326de6579bbbd26480
faiz <zynthkas@gmail.com>
2024-01-09 08:11:39 +0700

    add validator

--- src/Domains/DomainBase.js
+++ src/Domains/DomainBase.js
@@ -1,96 +1,15 @@
-class DomainBase {
-  #key;
+const Validator = require("./Validator");
 
-  #params;
+class DomainBase extends Validator {
+  constructor(payload) {
+    super(payload);
 
-  constructor(params) {
-    this.#params = params;
+    this._verifyPayload(payload);
+    this._payload = this.output();
   }
 
-  isRequired(key, format = null) {
-    // find params with key
-    const value = this.#params[key];
-
-    // if not found throw error
-    if (!value) {
-      throw new Error(`${this.#camelCaseToSnakeCase(key)}.INPUT_VALIDATION.IS_REQUIRED`);
-    }
-
-    this.#key = key;
-
-    // if found validate
-    this.#validate(value, format);
-  }
-
-  isOptional(key, format = null) {
-    // if not found dont validate
-    const value = this.#params[key];
-
-    if (!value) {
-      return;
-    }
-
-    this.#key = key;
-    // if found validate
-    this.#validate(value, format);
-  }
-
-  #validate(value, validator) {
-    if (validator === null) {
-      return;
-    }
-
-    // validate value with validator
-    if (validator === 'string') {
-      this.#validateString(value);
-    }
-
-    if (validator === 'number') {
-      this.#validateNumber(value);
-    }
-
-    if (validator === 'email') {
-      this.#validateEmail(value);
-    }
-
-    if (validator === 'date') {
-      this.#validateDateFormat(value);
-    }
-  }
-
-  #validateDateFormat(date) {
-    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-
-    if (!dateRegex.test(date)) {
-      throw new Error(`${this.#camelCaseToSnakeCase(this.#key)}.INPUT_VALIDATION.DATE_FORMAT_NOT_VALID`);
-    }
-  }
-
-  #validateNumber(value) {
-    const numberRegex = /^\d+$/;
-
-    if (!numberRegex.test(value)) {
-      throw new Error(`${this.#camelCaseToSnakeCase(this.#key)}.INPUT_VALIDATION.IS_NOT_NUMBER`);
-    }
-  }
-
-  #validateString(value) {
-    // validate single string value
-    if (typeof value !== 'string') {
-      throw new Error(`${this.#camelCaseToSnakeCase(this.#key)}.INPUT_VALIDATION.IS_NOT_STRING`);
-    }
-  }
-
-  #validateEmail(email) {
-    const emailRegex = /\S+@\S+\.\S+/;
-    if (!emailRegex.test(email)) {
-      throw new Error(`${email}.INPUT_VALIDATION.IS_NOT_EMAIL`);
-    }
-  }
-
-  #camelCaseToSnakeCase(camelCase) {
-    // convert camelCase to snake_case
-    return camelCase.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase();
+  _verifyPayload(payload) {
+    throw new Error("BASE_DOMAIN.METHOD_NOT_IMPLEMENTED");
   }
 }
 
---
+++ src/Domains/Validator.js
@@ -0,0 +1,127 @@
+const UnprocessableError = require("../Commons/exceptions/UnprocessableError");
+
+class Validator {
+  #key;
+  #validatedOutput;
+  #params;
+  #errors;
+
+  constructor(params) {
+    this.#params = params;
+    this.#validatedOutput = {};
+    this.#errors = {};
+  }
+
+  isRequired(key, format = null) {
+    // find params with key
+    const value = this.#params[key];
+
+    // if not found throw error
+    if (!value) {
+      this.#errors[key].push({
+        message: `${this.#camelCaseToSnakeCase(key)} is required`,
+      });
+    }
+
+    this.#key = key;
+
+    // if found validate
+    this.#validate(value, format);
+  }
+
+  isOptional(key, format = null) {
+    // if not found dont validate
+    const value = this.#params[key];
+
+    if (!value) {
+      return;
+    }
+
+    this.#key = key;
+    // if found validate
+    this.#validate(value, format);
+  }
+
+  #validate(value, validator) {
+    if (validator === null) {
+      return;
+    }
+
+    // validate value with validator
+    if (validator === "string") {
+      this.#validateString(value);
+    }
+
+    if (validator === "number") {
+      this.#validateNumber(value);
+    }
+
+    if (validator === "email") {
+      this.#validateEmail(value);
+    }
+
+    if (validator === "date") {
+      this.#validateDateFormat(value);
+    }
+
+    this.#validatedOutput[this.#key] = value;
+  }
+
+  #validateDateFormat(date) {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+    if (!dateRegex.test(date)) {
+      this.#errors[this.#key].push({
+        message: `${this.#camelCaseToSnakeCase(
+          this.#key
+        )} is not in date format`,
+      });
+    }
+  }
+
+  #validateNumber(value) {
+    const numberRegex = /^\d+$/;
+
+    if (!numberRegex.test(value)) {
+      this.#errors[this.#key].push({
+        message: `${this.#camelCaseToSnakeCase(this.#key)} is not a number`,
+      });
+    }
+  }
+
+  #validateString(value) {
+    // validate single string value
+    if (typeof value !== "string") {
+      this.#errors[this.#key].push({
+        message: `${this.#camelCaseToSnakeCase(this.#key)} is not a string`,
+      });
+    }
+  }
+
+  #validateEmail(email) {
+    const emailRegex = /\S+@\S+\.\S+/;
+
+    if (!emailRegex.test(email)) {
+      this.#errors[this.#key].push({
+        message: `${this.#camelCaseToSnakeCase(
+          this.#key
+        )} is not a valid email`,
+      });
+    }
+  }
+
+  #camelCaseToSnakeCase(camelCase) {
+    // convert camelCase to snake_case
+    return camelCase.replace(/([a-z])([A-Z])/g, "$1_$2").toUpperCase();
+  }
+
+  output = () => {
+    if (Object.keys(this.#errors).length > 0) {
+      throw new UnprocessableError(this.#errors);
+    }
+
+    return this.#validatedOutput;
+  };
+}
+
+module.exports = Validator;
