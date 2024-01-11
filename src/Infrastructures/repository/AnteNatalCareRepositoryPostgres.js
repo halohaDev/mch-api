8db575804a80e783b773a76e021f202977161155
faiz <zynthkas@gmail.com>
2024-01-12 03:04:22 +0700

    ante natal postgres impl

---
+++ src/Infrastructures/repository/AnteNatalCareRepositoryPostgres.js
@@ -0,0 +1,67 @@
+const AnteNatalCareRepository = require("../../Domains/ante_natal/AnteNatalCareRepository");
+
+class AnteNatalCareRepositoryPostgres extends AnteNatalCareRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async addAnteNatalCare(payload) {
+    const {
+      placementId,
+      contactType,
+      weight,
+      height,
+      hemoglobin,
+      bloodPressure,
+      fundalHeight,
+      fetalHeartRate,
+      usgCheckDate,
+      temprature,
+      action,
+      bloodType,
+      ttImunization,
+      proteinInUrine,
+      sugarInUrine,
+      hbsag,
+      hiv,
+      syphilis,
+      maternalHistoryId,
+    } = payload;
+
+    const id = `anc-${this._idGenerator()}`;
+
+    const query = {
+      text: "INSERT INTO ante_natal_cares(id, placement_id, contact_type, weight, height, hemoglobin, blood_pressure, fundal_height, fetal_heart_rate, usg_check_date, temprature, action, blood_type, tt_imunization, protein_in_urine, sugar_in_urine, hbsag, hiv, syphilis, maternal_history_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20) RETURNING id",
+      values: [
+        id,
+        placementId,
+        contactType,
+        weight,
+        height,
+        hemoglobin,
+        bloodPressure,
+        fundalHeight,
+        fetalHeartRate,
+        usgCheckDate,
+        temprature,
+        action,
+        bloodType,
+        ttImunization,
+        proteinInUrine,
+        sugarInUrine,
+        hbsag,
+        hiv,
+        syphilis,
+        maternalHistoryId,
+      ],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows[0].id;
+  }
+}
+
+module.exports = AnteNatalCareRepositoryPostgres;
---
+++ src/Infrastructures/repository/_test/AnteNatalCareRepositoryPostgres.test.js
@@ -0,0 +1,83 @@
+const pool = require("../../database/postgres/pool");
+const AnteNatalCareTableTestHelper = require("../../../../tests/AnteNatalCaresTableTestHelper");
+const AnteNatalCareRepositoryPostgres = require("../AnteNatalCareRepositoryPostgres");
+const UsersTableTestHelper = require("../../../../tests/UsersTableTestHelper");
+const NagariTableTestHelper = require("../../../../tests/NagariTableTestHelper");
+const JorongTableTestHelper = require("../../../../tests/JorongTableTestHelper");
+const PlacementTableTestHelper = require("../../../../tests/PlacementsTableTestHelper");
+const MaternalTableTestHelper = require("../../../../tests/MaternalTableTestHelper");
+const MaternalHistoryTableTestHelper = require("../../../../tests/MaternalHistoriesTableTestHelper");
+
+describe("AnteNatalCareRepositoryPostgres", () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await AnteNatalCareTableTestHelper.cleanTable();
+    await MaternalHistoryTableTestHelper.cleanTable();
+    await MaternalTableTestHelper.cleanTable();
+    await JorongTableTestHelper.cleanTable();
+    await NagariTableTestHelper.cleanTable();
+    await PlacementTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  beforeEach(async () => {
+    await UsersTableTestHelper.addUser({ id: "midwife-123" });
+    await JorongTableTestHelper.addJorong({ id: "jorong-123" });
+    await PlacementTableTestHelper.addPlacement({
+      midwifeId: "midwife-123",
+      jorongId: "jorong-123",
+    });
+
+    await UsersTableTestHelper.addUser({
+      id: "user-123",
+      email: "maternal@mail.com",
+    });
+    await MaternalTableTestHelper.addMaternal({ id: "maternal-123" });
+    await MaternalHistoryTableTestHelper.addMaternalHistory({
+      id: "maternal-history-123",
+      maternalId: "maternal-123",
+    });
+  });
+
+  describe("addAnteNatalCare function", () => {
+    it("should persist add antenatalcare and return antenatalcare id correctly", async () => {
+      // Arrange
+      const payload = {
+        maternalHistoryId: "maternal-history-123",
+        contactType: "c1",
+        action: "action",
+        ttImunization: "4",
+        height: 160,
+        hemoglobin: 12,
+        weight: 50,
+        bloodPressure: 120,
+        fundalHeight: 10,
+        fetalHeartRate: 120,
+        hbsag: "negative",
+        hiv: "negative",
+        syphilis: "negative",
+        bloodType: "A",
+        usgCheckDate: "2021-08-01",
+        placementId: "placement-123",
+      };
+
+      const fakeIdGenerator = () => "123";
+      const anteNatalCareRepositoryPostgres =
+        new AnteNatalCareRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const anteNatalCareId =
+        await anteNatalCareRepositoryPostgres.addAnteNatalCare(payload);
+
+      // Assert
+      const anteNatalCares =
+        await AnteNatalCareTableTestHelper.findAnteNatalCareById("anc-123");
+
+      expect(anteNatalCareId).toStrictEqual("anc-123");
+      expect(anteNatalCares).toBeDefined();
+    });
+  });
+});
