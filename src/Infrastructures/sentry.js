7fc64a13fcc4d487c5b61d1fa668e342f7656790
mnfaizp <zynthkas@gmail.com>
2024-01-18 23:40:56 +0700

    change default value

--- src/Infrastructures/sentry.js
+++ src/Infrastructures/sentry.js
@@ -2,6 +2,6 @@ import * as Sentry from "@sentry/node";
 
 module.exports = Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  environment: process.env.NODE_ENV || "development",
-  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE || 0.5,
+  environment: process.env.NODE_ENV || "production",
+  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE || 1,
 });
