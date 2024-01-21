af0397c82944fa8bd59d4e8302516be10ea5fa62
mnfaizp <zynthkas@gmail.com>
2024-01-21 08:29:10 +0700

    init implementation

--- src/Infrastructures/repository/ReportRepositoryPostgres.js
+++ src/Infrastructures/repository/ReportRepositoryPostgres.js
@@ -67,6 +67,53 @@ class ReportRepositoryPostgres extends ReportRepository {
 
     await this._pool.query(query);
   }
+
+  async calculateAnteNatalCareReport(queryParams) {
+    const { jorongId, startDate, endDate } = queryParams;
+
+    const selectQuery = `
+      SELECT 
+        COUNT(DISTINCT CASE WHEN hemoglobin < 8 THEN maternal_histories.id END) AS anemiaLessThan8,
+        COUNT(DISTINCT CASE WHEN hemoglobin >= 8 AND hemoglobin <= 11.9 THEN maternal_histories.id END) AS anemia8And11,
+        COUNT(DISTINCT CASE WHEN hemoglobin IS NOT NULL THEN maternal_histories.id END) AS hemoglobinCheck,
+        COUNT(DISTINCT CASE WHEN protein_in_urine = 'positive' THEN maternal_histories.id END) AS proteinInUrinePositive,
+        COUNT(DISTINCT CASE WHEN protein_in_urine IS NOT NULL THEN maternal_histories.id END) AS proteinInUrineCheck,
+        COUNT(DISTINCT CASE WHEN sugar_in_urine = 'positive' THEN maternal_histories.id END) AS bloodSugarMoreThan140,
+        COUNT(DISTINCT CASE WHEN sugar_in_urine IS NOT NULL THEN maternal_histories.id END) AS bloodSugarCheck,
+        COUNT(DISTINCT CASE WHEN hiv = 'positive' THEN maternal_histories.id END) AS comeWithHivPositive,
+        COUNT(DISTINCT CASE WHEN hiv IS NOT NULL THEN maternal_histories.id END) AS hivCheck,
+        COUNT(DISTINCT CASE WHEN hbsag = 'positive' THEN maternal_histories.id END) AS hepatitisPositive,
+        COUNT(DISTINCT CASE WHEN hbsag IS NOT NULL THEN maternal_histories.id END) AS hepatitisCheck,
+        COUNT(DISTINCT CASE WHEN syphilis = 'positive' THEN maternal_histories.id END) AS syphilisPositive,
+        COUNT(DISTINCT CASE WHEN syphilis IS NOT NULL THEN maternal_histories.id END) AS syphilisCheck
+      FROM ante_natal_cares
+    `;
+
+    let indexWhere = 0;
+    const jorongCondition = jorongId
+      ? `WHERE maternal_histories.jorong_id = $'${++indexWhere}'`
+      : "";
+    const dateCondition =
+      startDate && endDate
+        ? `AND maternal_histories.created_at BETWEEN $'${++indexWhere}' AND $'${++indexWhere}'`
+        : "";
+
+    const query = {
+      text: `
+        ${selectQuery}
+        INNER JOIN maternal_histories ON maternal_histories.id = ante_natal_cares.maternal_history_id
+        ${jorongCondition}
+        ${dateCondition}
+        GROUP BY maternal_histories.id
+      `,
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows[0];
+  }
+
+  async calculateAnteNatalCareReport(queryParams) {}
 }
 
 module.exports = ReportRepositoryPostgres;
