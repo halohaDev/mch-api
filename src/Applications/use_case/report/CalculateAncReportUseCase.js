95c7f02b59ded9722a86e0bddbec634b8aab4321
mnfaizp <zynthkas@gmail.com>
2024-01-26 04:12:50 +0700

    use case addition

---
+++ src/Applications/use_case/report/CalculateAncReportUseCase.js
@@ -0,0 +1,48 @@
+class CalculateAncReportUseCase {
+  constructor({ reportRepository }) {
+    this._reportRepository = reportRepository;
+  }
+
+  async execute(useCasePayload) {
+    const { jorongId, month, year } = useCasePayload;
+
+    const { startDate, endDate } = this._getStartAndEndDate(month, year);
+
+    const reports = await this._reportRepository.calculateAncReport({
+      jorongId,
+      startDate,
+      endDate,
+    });
+
+    return reports;
+  }
+
+  _getStartAndEndDate(month, year) {
+    if (month && year) {
+      // get first day of the month
+      const startDate = new Date(year, month - 1, 1);
+
+      // get last day of the month
+      const endDate = new Date(year, month, 0);
+
+      return { startDate, endDate };
+    }
+
+    if (year && !month) {
+      // get first day of the year
+      const startDate = new Date(year, 0, 1);
+      // get last day of the year
+      const endDate = new Date(year, 12, 0);
+
+      return { startDate, endDate };
+    }
+
+    // default to start and end of current month
+    const startDate = new Date();
+    startDate.setDate(1);
+    const endDate = new Date();
+    endDate.setMonth(endDate.getMonth() + 1);
+
+    return { startDate, endDate };
+  }
+}
---
+++ src/Applications/use_case/report/_test/CalculateAncReportUseCase.js
@@ -0,0 +1,152 @@
+const CalculateAncReportUseCase = require("./CalculateAncReportUseCase");
+const ReportRepository = require("../../Domains/reports/ReportRepository");
+
+describe("CalculateAncReportUseCase", () => {
+  it("should orchestrating the calculate anc report action correctly", async () => {
+    // Arrange
+    const useCasePayload = {
+      jorongId: "jorong-123",
+      month: 1,
+      year: 2021,
+    };
+
+    const mockReportRepository = new ReportRepository();
+
+    mockReportRepository.calculateAncReport = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        anemia_less_than_8: 0,
+        anemia_between_8_and_11: 0,
+        blood_sugar_check: 0,
+      })
+    );
+
+    const getAncReportUseCase = new CalculateAncReportUseCase({
+      reportRepository: mockReportRepository,
+    });
+
+    // Action
+    const reports = await getAncReportUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(reports).toStrictEqual({
+      anemia_less_than_8: 0,
+      anemia_between_8_and_11: 0,
+      blood_sugar_check: 0,
+    });
+    expect(mockReportRepository.calculateAncReport).toBeCalledWith({
+      jorongId: "jorong-123",
+      startDate: new Date(2021, 0, 1),
+      endDate: new Date(2021, 1, 0),
+    });
+  });
+
+  it("should orchestrating the calculate anc report action correctly when month and year not provided", async () => {
+    // Arrange
+    const useCasePayload = {
+      jorongId: "jorong-123",
+    };
+
+    const mockReportRepository = new ReportRepository();
+
+    mockReportRepository.calculateAncReport = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        anemia_less_than_8: 0,
+        anemia_between_8_and_11: 0,
+        blood_sugar_check: 0,
+      })
+    );
+
+    const getAncReportUseCase = new CalculateAncReportUseCase({
+      reportRepository: mockReportRepository,
+    });
+
+    // Action
+    const reports = await getAncReportUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(reports).toStrictEqual({
+      anemia_less_than_8: 0,
+      anemia_between_8_and_11: 0,
+      blood_sugar_check: 0,
+    });
+    expect(mockReportRepository.calculateAncReport).toBeCalledWith({
+      jorongId: "jorong-123",
+      startDate: new Date(),
+      endDate: new Date(),
+    });
+  });
+
+  it("should orchestrating the calculate anc report action correctly when year not provided", async () => {
+    // Arrange
+    const useCasePayload = {
+      jorongId: "jorong-123",
+      month: 1,
+    };
+
+    const mockReportRepository = new ReportRepository();
+
+    mockReportRepository.calculateAncReport = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        anemia_less_than_8: 0,
+        anemia_between_8_and_11: 0,
+        blood_sugar_check: 0,
+      })
+    );
+
+    const getAncReportUseCase = new CalculateAncReportUseCase({
+      reportRepository: mockReportRepository,
+    });
+
+    // Action
+    const reports = await getAncReportUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(reports).toStrictEqual({
+      anemia_less_than_8: 0,
+      anemia_between_8_and_11: 0,
+      blood_sugar_check: 0,
+    });
+    expect(mockReportRepository.calculateAncReport).toBeCalledWith({
+      jorongId: "jorong-123",
+      startDate: new Date(),
+      endDate: new Date(),
+    });
+  });
+
+  it("should orchestrating the calculate anc report action correctly when month not provided", async () => {
+    // Arrange
+    const useCasePayload = {
+      jorongId: "jorong-123",
+      year: 2021,
+    };
+
+    const mockReportRepository = new ReportRepository();
+
+    mockReportRepository.calculateAncReport = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        anemia_less_than_8: 0,
+        anemia_between_8_and_11: 0,
+        blood_sugar_check: 0,
+      })
+    );
+
+    const getAncReportUseCase = new CalculateAncReportUseCase({
+      reportRepository: mockReportRepository,
+    });
+
+    // Action
+    const reports = await getAncReportUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(reports).toStrictEqual({
+      anemia_less_than_8: 0,
+      anemia_between_8_and_11: 0,
+      blood_sugar_check: 0,
+    });
+    expect(mockReportRepository.calculateAncReport).toBeCalledWith({
+      jorongId: "jorong-123",
+      startDate: new Date(2021, 0, 1),
+      endDate: new Date(2021, 12, 0),
+    });
+  });
+});
