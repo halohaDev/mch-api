8481b5ea58959aab3415e2e81fc8db2cb9125763
mnfaizp <zynthkas@gmail.com>
2024-01-27 22:56:34 +0700

    refactor user validation

--- src/Domains/users/entities/CreateUser.js
+++ src/Domains/users/entities/CreateUser.js
@@ -1,84 +1,67 @@
-class CreateUser {
+const DomainBase = require("../../DomainBase");
+
+class CreateUser extends DomainBase {
   constructor(payload) {
-    this._verifyPayload(payload);
+    super(payload);
 
     const {
-      email, password, name, role,
-    } = payload;
-
-    switch (role) {
-      case 'mother':
-        this._verifyPayloadMother(payload);
-        break;
-      default:
-        break;
-    }
+      role,
+      email,
+      password,
+      name,
+      nik,
+      phoneNumber,
+      address,
+      birthplace,
+      dateOfBirth,
+      jobTitle,
+      religion,
+      isActiveBpjs,
+      bpjsKesehatanNumber,
+    } = this.output();
 
     this.role = role;
     this.email = email;
     this.password = password;
     this.name = name;
-    this.nik = payload.nik;
-    this.phoneNumber = payload.phoneNumber;
-    this.address = payload.address;
-    this.birthplace = payload.birthplace;
-    this.dateOfBirth = payload.dateOfBirth;
-    this.jobTitle = payload.jobTitle;
-    this.religion = payload.religion;
-    this.isActiveBpjs = payload.isActiveBpjs;
-    this.bpjsKesehatanNumber = payload.bpjsKesehatanNumber;
+    this.nik = nik;
+    this.phoneNumber = phoneNumber;
+    this.address = address;
+    this.birthplace = birthplace;
+    this.dateOfBirth = dateOfBirth;
+    this.jobTitle = jobTitle;
+    this.religion = religion;
+    this.isActiveBpjs = isActiveBpjs;
+    this.bpjsKesehatanNumber = bpjsKesehatanNumber;
   }
 
-  _verifyPayload({ email, password, name }) {
-    if (!email || !password || !name) {
-      throw new Error('CREATE_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+  _verifyPayload(payload) {
+    this.isRequired("role", "string");
+    this.isRequired("email", "string");
+    this.isRequired("password", "string");
+    this.isRequired("name", "string");
+
+    if (payload.role === "mother") {
+      this.isRequired("nik", "string");
+      this.isRequired("phoneNumber", "string");
+      this.isRequired("address", "string");
+      this.isRequired("birthplace", "string");
+      this.isRequired("dateOfBirth", "date");
+      this.isRequired("jobTitle", "string");
+      this.isRequired("religion", "string");
+      this.isRequired("isActiveBpjs", "boolean");
+      this.isRequired("bpjsKesehatanNumber", "string");
+    } else {
+      this.isOptional("nik", "string");
+      this.isOptional("phoneNumber", "string");
+      this.isOptional("address", "string");
+      this.isOptional("birthplace", "string");
+      this.isOptional("dateOfBirth", "date");
+      this.isOptional("jobTitle", "string");
+      this.isOptional("religion", "string");
+      this.isOptional("isActiveBpjs", "boolean");
+      this.isOptional("bpjsKesehatanNumber", "string");
     }
-
-    if (typeof email !== 'string' || typeof password !== 'string' || typeof name !== 'string') {
-      throw new Error('CREATE_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
-    }
-
-    if (!this._verifyEmail(email)) {
-      throw new Error('CREATE_USER.EMAIL_IS_NOT_VALID');
-    }
-  }
-
-  _verifyEmail(email) {
-    // eslint-disable-next-line no-control-regex
-    const emailRegex = /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
-    return emailRegex.test(email);
-  }
-
-  _verifyPayloadMother({
-    nik,
-    phoneNumber,
-    address,
-    birthplace,
-    dateOfBirth,
-    jobTitle,
-    religion,
-    isActiveBpjs,
-    bpjsKesehatanNumber,
-  }) {
-    if (
-      !nik || !phoneNumber || !address || !birthplace || !dateOfBirth
-      || !jobTitle || !religion || !isActiveBpjs || !bpjsKesehatanNumber
-    ) {
-      throw new Error('CREATE_USER.NOT_CONTAIN_NEEDED_PROPERTY');
-    }
-
-    if (typeof nik !== 'string' || typeof phoneNumber !== 'string' || typeof address !== 'string' || typeof birthplace !== 'string' || typeof dateOfBirth !== 'string' || typeof jobTitle !== 'string' || typeof religion !== 'string' || typeof isActiveBpjs !== 'boolean' || typeof bpjsKesehatanNumber !== 'string') {
-      throw new Error('CREATE_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
-    }
-
-    if (!this._verifyNikFormat(nik)) {
-      throw new Error('CREATE_USER.NIK_IS_NOT_VALID');
-    }
-  }
-
-  _verifyNikFormat(nik) {
-    const nikRegex = /^[0-9]{16}$/;
-    return nikRegex.test(nik);
   }
 }
 
