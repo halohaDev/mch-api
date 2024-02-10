116836f78500d02ccd9ed15f23fb006e7c8c7e61
faiz <zynthkas@gmail.com>
2024-02-10 12:14:58 +0700

    ante natal authorization

--- src/Interfaces/http/api/ante_natal/routes.js
+++ src/Interfaces/http/api/ante_natal/routes.js
@@ -3,11 +3,21 @@ const routes = (handler) => [
     method: "POST",
     path: "/api/v1/ante_natal_cares",
     handler: handler.postAnteNatalCareHandler,
+    options: {
+      app: {
+        access: ["admin", "midwife", "coordinator"],
+      },
+    },
   },
   {
     method: "GET",
     path: "/api/v1/ante_natal_cares",
     handler: handler.getAnteNatalCaresHandler,
+    options: {
+      app: {
+        access: ["admin", "midwife", "coordinator", "chairperson"],
+      },
+    },
   },
 ];
 
