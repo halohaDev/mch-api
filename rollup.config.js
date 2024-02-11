6cf9d831b9833ac75baec192866d9a47e523d03a
mnfaizp <mnfaizp@gmail.com>
2024-02-11 09:39:28 +0700

    config rollup to get module

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/app.js", // replace with path to your main server file
@@ -19,6 +20,7 @@ export default {
       exclude: ["node_modules/**", "**/_test/**"],
     }),
     json(),
+    terser(),
   ],
   external: ["aws-sdk", "nock", "mock-aws-s3", "cloudflare:sockets"],
 };
