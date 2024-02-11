5e71f0a2ded59adb2eb60872f761fdf199b78b8b
mnfaizp <mnfaizp@gmail.com>
2024-02-11 09:26:22 +0700

    setup rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
+import json from "@rollup/plugin-json";
 
 export default {
   input: "src/app.js", // replace with path to your main server file
@@ -10,13 +11,14 @@ export default {
     sourcemap: true, // generate source maps
   },
   plugins: [
-    externals(),
     resolve({
       preferBuiltins: true,
     }),
     commonjs(),
     babel({
-      exclude: "node_modules/**",
+      exclude: ["node_modules/**", "**/_test/**"],
     }),
+    json(),
   ],
+  external: ["aws-sdk", "nock", "mock-aws-s3", "cloudflare:sockets"],
 };
