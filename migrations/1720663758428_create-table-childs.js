f7fd5e82a2fd3b53cfef17581611fe64c73a8a02
mnfaizp <zynthkas@gmail.com>
2024-07-11 09:26:10 +0700

    migrate childs

---
+++ migrations/1720663758428_create-table-childs.js
@@ -0,0 +1,84 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createType("delivery_places", [
+    "RS",
+    "PUSKESMAS",
+    "RUMAH",
+    "BPS",
+    "POLINDES",
+    "PUSTU",
+    "KLINIK",
+    "POSKESRI",
+  ]);
+  pgm.createType("delivery_methods", ["NORMAL", "SC", "VAKUM", "FORCEPS"]);
+  pgm.createType("helpers", ["DOKTER", "BIDAN", "PERAWAT", "DUKUN"]);
+
+  pgm.createTable("maternal_complications", {
+    id: {
+      type: "varchar",
+      primaryKey: true,
+    },
+    name: {
+      type: "varchar",
+      notNull: true,
+    },
+    nik: {
+      type: "varchar(16)",
+      notNull: true,
+    },
+    birth_datetime: {
+      type: "datetime",
+      notNull: true,
+    },
+    birth_weight: {
+      type: "float",
+      notNull: true,
+    },
+    birth_height: {
+      type: "float",
+      notNull: true,
+    },
+    gender: {
+      type: "varchar(1)",
+      notNull: true,
+    },
+    father_name: {
+      type: "varchar",
+    },
+    pregnancy_age: {
+      type: "int",
+      notNull: true,
+    },
+    delivery_place: {
+      type: "delivery_places",
+      notNull: true,
+    },
+    delivery_method: {
+      type: "delivery_methods",
+      notNull: true,
+    },
+    helper: {
+      type: "helpers",
+      notNull: true,
+    },
+    maternal_id: {
+      type: "varchar",
+      notNull: true,
+      references: '"maternals"',
+      onDelete: "cascade",
+    },
+    maternal_history_id: {
+      type: "varchar",
+      references: '"maternal_histories"',
+      onDelete: "cascade",
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("maternal_complications");
+  pgm.dropType("delivery_places");
+  pgm.dropType("delivery_methods");
+  pgm.dropType("helpers");
+};
