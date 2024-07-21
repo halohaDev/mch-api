dfe87504dda5f83c75578da63db103c0932582b3
mnfaizp <zynthkas@gmail.com>
2024-07-21 08:30:11 +0700

    reset state order

--- src/Infrastructures/queries/BaseQuery.js
+++ src/Infrastructures/queries/BaseQuery.js
@@ -48,6 +48,7 @@ class BaseQuery {
       values: [],
       paginate: "",
       joins: "",
+      order: "",
       currentIndex: 0,
     };
 
@@ -55,7 +56,7 @@ class BaseQuery {
       text: this.finalSQL,
       values: values,
     };
-
+    console.log(query);
     const results = await this._pool.query(query);
 
     this._page = 1;
